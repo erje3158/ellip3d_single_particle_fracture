0613081d22666f19ad8bff363daa93651443cfcc
Erik Jensen <jensen.erik.w@gmail.com>
2018-04-18 14:33:58 -0600

    Updating make system for excalibur

--- source/Make_Script.sh
+++ source/Make_Script.sh
@@ -7,78 +7,13 @@ if [ "$#" = 1 ]
 	if [ "$1" = "excalibur" ]
 		then
 			echo "Compiling Hierarchical Upscaling Code on excalibur.arl.hpc.mil"
-			cp excalibur/Makefile ./
 
 			module swap PrgEnv-intel PrgEnv-gnu
 			module list
 
-			cd ../lib
-			export LD_LIBRARY_PATH="$(pwd):$LD_LIBRARY_PATH"
-			echo $LD_LIBRARY_PATH
-			cd ../source
-
 			rm *.o *~ hu_code
 			make
 			echo "Done!"
-
-			rm Makefile 
-
-	elif [ "$1" = "topaz" ]
-		then
-			echo "Compiling Hierarchical Upscaling Code on topaz.erdc.hpc.mil"
-			cp topaz/Makefile ./
-
-			module unload compiler/intel/16.0.0
-			module load compiler/gcc/6.1.0
-			module list
-
-			cd ../lib
-			MKLA=/p/home/apps/intel/parallel_studio_2017_u1/compilers_and_libraries_2017.1.132/linux/compiler/lib/intel64_lin/
-			MKLB=/p/home/apps/intel/parallel_studio_2016/compilers_and_libraries_2016.0.109/linux/mkl/lib/intel64_lin/
-			export LD_LIBRARY_PATH="$(pwd):$MKLA:$MKLB:$LD_LIBRARY_PATH"
-			echo $LD_LIBRARY_PATH
-			cd ../source
-
-			rm *.o *~ hu_code
-			make
-			echo "Done!"
-
-			rm Makefile
-
-	elif [ "$1" = "macOS" ]
-		then
-			echo "Compiling Hierarchical Upscaling Code on macOS"
-			cp macOS/Makefile ./
-
-			cd ../lib
-			export LD_LIBRARY_PATH="$(pwd):$LD_LIBRARY_PATH"
-			echo $LD_LIBRARY_PATH
-			cd ../source
-
-			rm *.o *~ hu_code
-			make
-			echo "Done!"
-
-			rm Makefile
-
-	elif [ "$1" = "soilblast" ]
-		then
-			echo "Compiling Hierarchical Upscaling Code on soilblast2.colorado.edu"
-			cp soilblast/Makefile ./
-
-			module load gcc-6.4.0
-			module list
-
-			cd ../lib
-			export LD_LIBRARY_PATH="$(pwd):$LD_LIBRARY_PATH"
-			echo $LD_LIBRARY_PATH
-			cd ../source
-
-			rm *.o *~ hu_code
-			make
-			echo "Done!"
-
-			rm Makefile 
 
 	else
 		echo "Don't recognize that platform..."
